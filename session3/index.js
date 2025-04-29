6cb3ea86eb543fe371d11258aeee281a17078346
haimin13 <gkals862@naver.com>
2025-04-29 14:39:22 +0900

    add session3 folder and gitignore file

---
+++ session3/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = require('./router');
+const port = 3000;
+
+const app = express();
+
+
+/*
+app.use((req, res, next) => {
+    const randomNumber = Math.floor(Math.random() * 10);
+    console.log(`Random Number: ${randomNumber}`);
+    if (randomNumber % 3) return next();
+    else return res.sendStatus(403)
+});
+
+app.use((req, res, next) => {
+    const { method, path } = req;
+    console.log(`${method} ${path}`);
+    return next();
+});
+
+app.get('/', (req, res) => res.send('GET /'));
+
+app.post('/', (req, res) => res.send('POST /'));
+*/
+app.use('/', router);
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
